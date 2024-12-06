59e2872822e0135e755c617f403d4bfba4512b24
Paul Barry <paul.barry@wealthbox.com>
2024-12-06 09:27:21 -0500

    Resolve issue associating models

--- toy_app/app/models/micropost.rb
+++ toy_app/app/models/micropost.rb
@@ -1,4 +1,5 @@
 class Micropost < ApplicationRecord
-  belongs_to :user, class_name: "user", foreign_key: "user_id"
-  validates :content, length: { maximum: 140 }
+  belongs_to :user, class_name: "User", foreign_key: "user_id"
+  validates :content, length: { maximum: 140 }, presence: true
+  validates :user_id, presence: true
 end
--- toy_app/app/models/user.rb
+++ toy_app/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  has_many :microposts
+  has_many :microposts, class_name: "Micropost", foreign_key: "user_id"
+  validates :name, presence: true
+  validates :email, presence: true
 end
